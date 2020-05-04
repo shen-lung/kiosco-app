01d553e67fad0f448738c9e388f55c13f36438a7
vadim <banzabit@gmail.com>
2020-05-04 00:50:38 -0300

    Add base configuration with webpack

---
+++ react/src/App.js
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react';
+// import {Router} from 'react-router';
+
+import Home from './components/Home';
+
+function App() {
+    return (
+        <div>
+            <Home />
+        </div>
+    );
+}
+
+export default App;
---
+++ react/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { PureComponent} from 'react';
+
+class Home extends PureComponent {
+
+    render() {
+
+        return (
+            <div className="">
+               <h1>KIOSCO VIRTUAL</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
